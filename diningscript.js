24e951240b0ff80858a4dd8722fcdde5de79f8ed
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2019-10-13 04:10:10 -0500

    Update diningscript.js

--- diningscript.js
+++ diningscript.js
@@ -15,15 +15,18 @@ function accessWeb(){
 }
 
 var DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var SERVERIES = ["baker", "north", "west", "south", "seibel", "sid"];
+var TEST_ORDER = [3, 4, 5, 6, 0, 1, 2];
 
 function valid_menu_item(text){
+	text = text.trim();
 	if(text.includes("=")){
 		return false;
 	}
 	if(text.length <= 3){
 		return false;
 	}
-	if(text == "Lunch" || text == "Dinner"){
+	if(text == "Lunch Menu" || text == "Dinner Menu"){
 		return false;
 	}
 	for(var j = 0; j < 7; j++){
@@ -37,7 +40,21 @@ function valid_menu_item(text){
 	return true;
 }
 
+function capitalizeFirstLetter(string){
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function accessMenu(url){
+	var servery;
+	for(var i = 0; i < SERVERIES.length; i++){
+		if(url.toLowerCase().includes(SERVERIES[i])){
+			servery = capitalizeFirstLetter(SERVERIES[i]);
+			i = SERVERIES.length;
+		}
+	}
+	if(servery == "Sid"){
+		servery = "Sid Rich";
+	}
 	var pdfjsLib = window['pdfjs-dist/build/pdf'];
 	pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
 	var loadingTask = pdfjsLib.getDocument(url);
@@ -50,40 +67,53 @@ function accessMenu(url){
 					var menu = [[], [], [], [], [], [], []];
 					var x = [];
 					var y = [];
-					console.log(textContent);
+					var meal = "NONE";
+					var days = [];
+					for(var i = 0; i < 7; i++){
+						days.push({day:DAYS[i], index:i});
+					}
 					for(var i = 0; i < textContent.length; i++){
 						var text = textContent[i];
-						for(var j = 0; j < 7; j++){
-							if(text.str == DAYS[j]){
-								x[j] = text.transform[4];
-								y[j] = text.transform[5];
+						for(var j = 0; j < days.length; j++){
+							if(text.str == days[j].day){
+								x[days[j].index] = text.transform[4] + text.width / 2;
+								y[days[j].index] = text.transform[5];
+								days.splice(j, 1);
+								if(days.length == 0 && meal != "NONE"){
+									i = textContent.length;
+								}
+							}
+						}
+						if(text.str.includes("Menu")){
+							if(text.str.toLowerCase().trim() == "lunch menu"){
+								meal = "Lunch";
+							}
+							else if(text.str.toLowerCase().trim() == "dinner menu"){
+								meal = "Dinner";
 							}
 						}
 					}
 					for(var i = 0; i < textContent.length; i++){
 						if(valid_menu_item(textContent[i].str)){
-							console.log("VALID: ", textContent[i].str);
 							for(var j = 0; j < 7; j++){
-								if(textContent[i].transform[5] < y[j] && textContent[i].transform[4] < x[j]){
+								var index = TEST_ORDER[j]
+								if(textContent[i].transform[5] < y[index] && textContent[i].transform[4] < x[index]){
 									var insert_index = 0;
-									for(var h = 0; h < menu[j].length; h++){
-										if(menu[j][h].y > textContent[i].transform[5]){
+									for(var h = 0; h < menu[index].length; h++){
+										if(menu[index][h].y > textContent[i].transform[5]){
 											insert_index += 1;
 										}
 										else{
-											h = menu[j].length;
+											h = menu[index].length;
 										}
 									}
-									menu[j].splice(insert_index, 0, {str:textContent[i].str, y:textContent[i].transform[5]});
-									
+									menu[index].splice(insert_index, 0, {str:textContent[i].str, y:textContent[i].transform[5]});
+									j = 7;
 								}
 							}
 						}
-						else{
-							console.log("INVALID: ", textContent[i].str);
-						}
 					}
-					console.log(menu);
+					console.log(servery + " " + meal + ": ", menu);
 				});
 			});
 		}
