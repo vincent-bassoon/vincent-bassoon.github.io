4ca777c1ca91f15d24253158a09ac1f74bee1288
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2019-10-27 00:25:48 -0500

    Update diningscript.js

--- diningscript.js
+++ diningscript.js
@@ -323,6 +323,8 @@ function configure_ui(final_menu){
 		});
 	}	
 	day_buttons[current_day].classList.toggle("activeTab");
+	var temp = day_buttons[current_day].innerText;
+	day_buttons[current_day].innerText = "<b>" + temp + "</b>";
 	
 	var meal_buttons = [];
 	for(var i = 0; i < 3; i++){
@@ -335,6 +337,8 @@ function configure_ui(final_menu){
 		});
 	}
 	meal_buttons[current_meal].classList.toggle("activeTab");
+	var temp = meal_buttons[current_meal].innerText;
+	meal_buttons[current_meal].innerText = "<b>" + temp + "</b>";
 	
 	var acc = document.getElementsByClassName("accordion");
 	for(var i = 0; i < acc.length; i++) {
