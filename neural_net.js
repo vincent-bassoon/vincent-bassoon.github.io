e0a7956f6b55b450a7c27b55ecfbdfb5688e7d0e
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-01-03 12:19:06 -0500

    Create neural_net.js

---
+++ neural_net.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+apiKey: "AIzaSyBKWikmtDevji69aJ82dWyFuUrRHVAguvI",
+    authDomain: "neural-net-3da45.firebaseapp.com",
+    databaseURL: "https://neural-net-3da45.firebaseio.com",
+    projectId: "neural-net-3da45",
+    storageBucket: "neural-net-3da45.appspot.com",
+    messagingSenderId: "114093697431",
+    appId: "1:114093697431:web:51fb1b15fac0e498ebbcd2",
+    measurementId: "G-BSM3T5J3ZD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
