ab649ecf8a9fec9643b5c01fed3314ad44bb5466
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-04-29 16:49:27 -0400

    Update corruptorscript.js

--- corruptorscript.js
+++ corruptorscript.js
@@ -1,43 +1,49 @@
-function byte_pairs(dict){
-    console.log(dict);
-}
-
-function process(text, dict, done, letter_index){
-	var data = text.split(")");
-	var temp;
-	data.splice(data.length - 1, 1);
-	for(var i = 0; i < data.length; i++){
-		temp = data[i].split(" (");
-		dict[temp[0]] = parseInt(temp[1]);
-	}
-	done[letter_index] = true;
-	for(var i = 0; i < 26; i++){
-		if(!(i in done && done[i])){
-			return;
+function create_byte_pairs(){
+	var x = new XMLHttpRequest();
+	x.open("GET", "https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/earonesty/dotfiles/master/frequent.js");
+	x.onreadystatechange = function(){
+		if(x.readyState == 4 && x.status == 200){
+ 			var response = JSON.parse(x.responseText);
+			var word_list = [];
+			for(var word in response){
+				word_list.push({"word":("." + word + ".").split(""), freq:response[word]});
+			}
+			var byte_pairs = [];
+			for(var i = 0; i < 1000; i++){
+				var pairs = {};
+				for(var j = 0; j < word_list.length; j++){
+					var word = word_list[j].word;
+					for(var k = 0; k < word.length - 1; k++){
+						var pair = word[k] + word[k + 1];
+						if(pair in pairs){
+							pairs[pair] += word_list[j].freq;
+						}
+						else{
+							pairs[pair] = word_list[j].freq;
+						}
+					}
+				}
+				var best_pair = null;
+				var best_pair_freq = -1;
+				for(pair in pairs){
+					if(pairs[pair] > best_pair_freq){
+						best_pair = pair;
+						best_pair_freq = pairs[pair];
+					}
+				}
+				console.log("New Pair: ", best_pair);
+				byte_pairs.push(best_pair);
+				for(var j = 0; j < word_list.length; j++){
+					var word = word_list[j].word;
+					for(var k = 0; k < word.length - 1; k++){
+						if(word[k] + word[k + 1] == best_pair){
+							word[k] = word[k] + word.splice(k + 1, 1);
+						}
+					}
+				}
+			}
+			console.log(byte_pairs);
 		}
-	}
-	byte_pairs(dict);
-}
-
-function get_words(){
-	var dict = {};
-	var done = [];
-	var start_key = "of each word form.";
-	var end_key = "Back to the concordance menu";
-	for(var i = 0; i < 26; i++){
-		done[i] = false;
-		var x = new XMLHttpRequest();
-		x.open("GET", "https://cors-anywhere.herokuapp.com/https://www.opensourceshakespeare.org/concordance/wordformlist.php?Letter=" + 
-			   String.fromCharCode(i + 65) + "&pleasewait=1&msg=sr");
-		x.onreadystatechange = function(){
-			console.log(String.fromCharCode(i + 65), "ATTEMPT");
-			if(x.readyState == 4 && x.status == 200){
-				console.log(String.fromCharCode(i + 65), "SUCCESS");
-				var text = new DOMParser().parseFromString(x.responseText, "text/html").documentElement.innerText;
-				text = text.substring(text.indexOf(start_key) + start_key.length, text.indexOf(end_key)).trim();
-                process(text, dict, done, i);
-	    	}
-	    };
-	    x.send(null);
-    }
+	};
+	x.send(null);
 }
