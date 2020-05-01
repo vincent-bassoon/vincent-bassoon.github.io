107c263862052b0b932d32b45b21a449a879e54b
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-04-30 22:42:43 -0400

    Update bachscript.js

--- bachscript.js
+++ bachscript.js
@@ -1,22 +1,69 @@
-// Decide on structure
+function run(){
+	// Decide basic structure
+	var modality = choose({"Major":0.5, "Minor":0.5});
+	var cadence_num = choose({4:0.8, 5:0.2});
+	//  If 5 segments, guaranteed 7-8 segment length?
+	var pickup = choose({1:0.667, 0:0.333});
+	
+	var segments = [];
+	for(var index = 0; index < cadence_num; index++){
+		var length = pickup + choose({7:0.9, 9:0.1});
+		segments.push(generate_segment(length, index));
+	}
+	
+	
+}
 
-// Major or minor: 50% M, 50% m
+function choose(probs){
+	var num = Math.random();
+	var choice = null;
+	var sum = 0;
+	for(var key in probs){
+		sum += probs[key];
+		if(sum > num && choice == null){
+			choice = key;
+		}
+	}
+	if(choice == null){
+		console.log("Probability null choice error: ", probs);
+	}
+	if(sum != 1.0){
+		console.log("Probability sum error: ", probs);
+	}
+	return choice;
+}
 
-// 4-5 segments: 80% 4, 20% 5
-//   If 5 segments, guaranteed 7-8 segment length.
-
-// Segment length: 90% 7-8, 10% 9-10
-//    cadence on first or third beat tied to segment length
-
-// pickup or not: 66.7% yes, 33.3% no
-//   If not pickup, then no other segment will have 1 note pickup
 //   If not pickup, then 2 note pickup after downbeat cadence
 //   If pickup, then 1 note pickup after downbeat cadence
 
+// Key patterns
+// Major:
+//   Tonic: I, vi, iii
+//   Dominant: V, vii
+//   Predominant: ii, IV
+// Minor:
+//   Tonic: i, III, VI, VII
+//   Dominant: V, vii
+//   Predominant: VI, iv
 
+// I: ALL
+// II: V, vii
+// III: IV, VI
+// IV: II, V, vii, I
+// V: I, vii, VI, IV, II
+// VI: IV, II, III, V
+// vii: I, V
+// VII: III, IV, II
+
+
+// Decide chords
 
 // Decide on chords for cadences: 74% PAC/IAC, 17% HC, 7% DC, 2% PC
 //   Ending: 90% PAC, 10% IAC ... 70% Piccardy third for minor, 30% not
+
+// Starting chord: 70% V, 30% I
+
+
 
 // Construct melody
 // PAC/IAC: 55% 2-1, 14% 4-3, 13% 7-1, 12% 2-3, 3% 5-3, 3% 5-5
@@ -25,8 +72,8 @@
 // PC: 50% 1-1, 50% 6-5
 
 
-// Decide chords
 // Harmonize
+
 
 
 // type = major, minor, aug, dim, 7, d7
