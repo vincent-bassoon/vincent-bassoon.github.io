1b291c6abf14d38b1f4b830c6907cb5f4eaace54
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-05-07 15:36:32 -0400

    Update bachscript.js

--- bachscript.js
+++ bachscript.js
@@ -34,18 +34,17 @@ function choose_from_freqs(freqs, choices){
 	return null;
 }
 
-function generate_chorale_plan(key, modality, cadence_num){
+function generate_chorale_plan(key, cadence_num){
 	var lengths = {"pac": 3, "pac/iac": 3, "hc": 2, "dc": 3, "pc": 2, "pacm": 3};
 	var endings = {"pac": 1, "pac/iac": 1, "hc": 5, "dc": 6, "pc": 1, "pacm": 1};
 	var chorale_plan = [];
 	var previous_cadence_chord = null;
 	for(var i = 0; i < cadence_num; i++){
-		var modulation = {"key": key, "modality": modality};
 		var cadence;
 		
 		// Ending: 100% PAC ... 70% Piccardy third for minor, 30% not
 		if(i == cadence_num - 1){
-			if(modality == "minor"){
+			if(key.get_modality() == "minor"){
 				cadence = choose({"pac": 0.3, "pacm": 0.7});
 			}
 			else{
@@ -59,31 +58,40 @@ function generate_chorale_plan(key, modality, cadence_num){
 		
 		var cadence_length = lengths[cadence];
 		// 4 beat cadence includes a 64 tonic
-		if(cadence != "pc" && choose({false: 0.8, true: 0.2})){
+		if(cadence != "pac/iac" && cadence_length == 3 && choose({false: 0.8, true: 0.2})){
 			cadence_length++;
 		}
-		chorale_plan.push(new PhraseData(modulation, cadence, cadence_length, previous_cadence_chord));
+		chorale_plan.push(new PhraseData(key, cadence, cadence_length, previous_cadence_chord));
 		previous_cadence_chord = endings[cadence];
 	}
 	return chorale_plan;
 }
 
+class Key {
+	constructor(pitch, modality){
+		this.pitch = pitch;
+		this.modality = modality;
+	}
+	get_pitch(){return this.pitch;}
+	get_modality(){return this.modality;}
+}
+
 class PhraseData {
 	// note: previous_cadence_chord means be careful updating a single PhraseData object without updating them all
-	constructor(modulation, cadence, cadence_length, previous_cadence_chord){
-		this.modulation = modulation;
+	constructor(key, cadence, cadence_length, previous_cadence_chord){
+		this.key = key;
 		this.cadence = cadence;
 		this.cadence_length = cadence_length;
 		this.previous_cadence_chord = previous_cadence_chord;
 	}
-	get_modulation(){return this.modulation;}
+	get_key(){return this.key;}
 	get_cadence(){return this.cadence;}
 	get_cadence_length(){return this.cadence_length;}
 	get_previous_cadence_chord(){return this.previous_cadence_chord;}
 }
 
 function run(){
-	var chord_data = new ChordData();
+	var chord_functions = new ChordFunctions();
 	// Decide basic structure
 	
 	// 50% major, 50% minor
@@ -98,38 +106,61 @@ function run(){
 	var sharp_or_flat = choose({0: 0.5, 2: 0.5}) - 1;
 	var pitch = (7 * (12 + (sharp_or_flat * num_accidentals))) % 12;
 	
-	var chorale_plan = generate_chorale_plan(pitch, modality, cadence_num);
+	var chorale_plan = generate_chorale_plan(new Key(pitch, modality), cadence_num);
 	
 	var segments = [];
 	for(var i = 0; i < cadence_num; i++){
 		// 90% 7-8 note segment length, 10% 9-10 note length
 		var length = pickup + choose({7: 0.8, 9: 0.2});
-		segments.push(generate_segment(length, segments, chorale_plan[i], chord_data));
+		segments.push(generate_segment(length, segments, chorale_plan[i], chord_functions));
 	}
 	
 	
 }
 
-function generate_segment(length, previous_segments, phrase_data, chord_data){
+function generate_segment(length, previous_segments, phrase_data, chord_functions){
 	var voices = [];
 	for(var i = 0; i < length; i++){
 		voices.push(null);
 	}
 }
 
-function generate_chords(length, phrase_data, chord_data){
+function generate_chords(length, phrase_data, chord_functions){
 	var sub_phrase_lengths = generate_sub_phrases(length, phrase_data);
+	var chords = [];
+	for(var i = 0; i < length; i++){
+		chords.push(null);
+	}
+	var key = phrase_data.get_key();
 	
-	/*for(var i = max - 1; i >= min; i--){
-		var cad_index = chords.length - 1 - i;
-		if(cad_index < phrase_data.get_cadence_length()){
-			chords[i] = chord_data.choose_cad_chord(cad_index, phrase_data);
-		}
-		else{
-			chords[i] = chord_data.generate_chord_before(chords[i + 1], phrase_data);
-		}
-	}*/
+	if(sub_phrase_lengths[0] == 1){
+		chords[0] = chord_functions.generate_chord(1, key, null);
+	}
+	else if(sub_phrase_lengths[0] == 2){
+		chords[0] = chord_functions.generate_chord(5, key, null);
+		chords[1] = chord_functions.generate_chord(1, key, null);
+	}
+	else{
+		console.log("first sub phrase length error: ", sub_phrase_lengths[0]);
+	}
 	
+	var cadence_chords = chord_functions.generate_cadence_chords(phrase_data.get_cadence(),
+								     phrase_data.get_cadence_length(),
+								     key);
+	for(var i = 0; i < cadence_chords.length; i++){
+		chords[length - 1 - 0] = cadence_chords[i];
+	}
+	
+	var chord_class = -1;
+	for(var i = 0; i < length; i++){
+		if(chord_class == -1){
+			chord_class = sub_phrase_lengths.shift() - 1;
+		}
+		if(chords[i] == null){
+			chords[i] = chord_functions.get_chord(chord_class, key);
+		}
+		chord_class--;
+	}
 }
 
 function generate_sub_phrases(length, phrase_data){
@@ -198,14 +229,56 @@ function generate_sub_phrases(length, phrase_data){
 	return sub_phrase_lengths;
 }
 
-
-
-// Decide chords
-
-
-
-
-
+class ChordFunctions {
+	constructor(){
+		// 0 is tonic, 1 is dominant, sub-dominant, tonic prolongation, tonic prolongation+
+		this.probs = {1: {5: 0.95, 7: 0.05}, 2: {2: 0.65, 4: 0.35}};
+		this.constants = {0: 1, 3: 6, 4: 3};
+		this.cadences = {"pac": [1, 5], "pac/iac": [1], "hc": [5], "dc": [6, 5], "pc": [1, 4], "pacm": [1, 5]};
+		this.modalities = {"major": {1: "major", 2: "minor", 3: "minor", 4: "major", 5: "major", 6: "minor", 7: "dim"},
+				   "minor": {1: "minor", 2: "dim", 3: "major", 4: "minor", 5: "major", 6: "major", 7: "dim"}};
+	}
+	generate_chord(roman_num, key, inversion){
+		return new Chord(roman_num, key, this.modalities[key.get_modality()][roman_num], inversion);
+	}
+	get_chord(chord_class, key){
+		if(chord_class == 1 || chord_class == 2){
+			return this.generate_chord(choose(this.probs[chord_class]), key, null);
+		}
+		else{
+			return this.generate_chord(this.constants[chord_class], key, null);
+		}
+	}
+	// returns chords of the cadence, last chord is at index 0
+	generate_cadence_chords(cad, cad_length, key){
+		var cadence_chords = [];
+		for(var i = 0; i < this.cadences[cad].length; i++){
+			if(cad == "pacm" && i == 0){
+				cadence_chords.push(new Chord(this.cadences[cad][i], key, "major", 0));
+			}
+			else{
+				cadence_chords.push(this.generate_chord(this.cadences[cad][i], key, 0));
+			}
+		}
+		if(cad_length == 4){
+			cadence_chords.push(this.generate_chord(1, key, 2));
+		}
+		return cadence_chords;
+	}
+}
+					
+class Chord {
+	constructer(roman_num, key, chord_modality, inversion){
+		this.roman_num = roman_num;
+		this.key = key;
+		this.chord_modality = chord_modality;
+		this.inversion
+	}
+	get_roman_num(){return this.roman_num;}
+	get_key(){return this.key;}
+	get_modality(){return this.chord_modality;}
+	get_inversion(){return this.inversion;}
+}
 
 // Construct melody
 // PAC/IAC: 55% 2-1, 14% 4-3, 13% 7-1, 12% 2-3, 3% 5-3, 3% 5-5
@@ -220,69 +293,6 @@ function generate_sub_phrases(length, phrase_data){
 //   If not pickup, then 2 note pickup after downbeat cadence
 //   If pickup, then 1 note pickup after downbeat cadence
 
-class ChordFunctions {
-	constructor(){
-		// 0 is tonic, 1 is dominant, sub-dominant, tonic prolongation, tonic prolongation+
-		this.probs = {1: {5: 0.95, 7: 0.05}, 2: {2: 0.65, 4: 0.35}};
-		this.constants = {0: 1, 3: 6, 4: 3};
-	}
-	get_chord(chord_class){
-		if(chord_class == 1 || chord_class == 2){
-			return choose(this.probs[chord_class]);
-		}
-		else{
-			return this.constants[chord_class];
-		}
-	}
-}
-
-class ChordData {
-	constructor(){
-		this.chord_functions = new ChordFunctions();
-	}
-	// note: index is from the end (0 is the last chord)
-	choose_cad_chord(index, phrase_data){
-		var key_data = phrase_data.get_modulation();
-		var cad = phrase_data.get_cadence();
-		if(index == 2){
-			return new Chord(this.chord_functions.get_chord(1), key_data.key, key_data.modality);
-		}
-		else if(index == 1){
-			if(cad == "pc"){
-				return new Chord(4, key_data.key, key_data.modality);
-			}
-			else{
-				return new Chord(choose({5: 0.97, 7: 0.03}), key_data.key, key_data.modality);
-			}
-		}
-		else if(index == 0){
-			if(cad == "pacm"){
-				return new Chord(1, key_data.key, "major");
-			}
-			else if(cad.includes("pac") || cad == "pc"){
-				return new Chord(1, key_data.key, key_data.modality);
-			}
-			else if(cad == "hc"){
-				return new Chord(5, key_data.key, key_data.modality);
-			}
-		}
-		else{
-			console.log("Cadence index error: ", index);
-			return null;
-		}
-	}
-}
-
-class Chord {
-	constructer(roman_num, key, modality){
-		this.roman_num = roman_num;
-		this.key = key;
-		this.modality = modality;
-	}
-	get_roman_num(){return this.roman_num;}
-	get_key(){return this.key;}
-	get_modality(){return this.modality;}
-}
 
 class Note {
 	constructer(pitch, octave){
