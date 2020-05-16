f89e788cd15a0ef3b8c3fe22bba8870c5225b80b
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-05-16 00:25:07 -0400

    Update bachscript_harmony.js

--- bachscript_harmony.js
+++ bachscript_harmony.js
@@ -1,20 +1,8 @@
-/*
-0: C
-2: D
-4: E
-5: F
-7: G
-9: A
-11: B
-
-*/
-
-
 class HarmonyFunctions {
 	constructor(){
 		this.name_to_num = {"C": 0, "D": 2, "E": 4, "F": 5, "G": 7, "A": 9, "B": 11};
 		function note(name, octave){
-			return new Note(this.name_to_num, octave);
+			return new Note(this.name_to_num[name], octave);
 		}
 		
 		this.preferred_ranges = {0: {"min": note("F", 2), "max": note("F", 3)},
@@ -27,7 +15,53 @@ class HarmonyFunctions {
 					2: {"min": note("G", 3), "max": note("D", 5)},
 					3: {"min": note("C", 4), "max": note("G", 5)}};
 	}
-	generate_harmony(chords, phrase_plan){
+	generate_adjacent(chords, harmony, adjacent_index, target_index){
+		if(Math.abs(adjacent_index - target_index) != 1){
+			console.log("index error");
+			return;
+		}
+		
+	}
+	complete_harmony(chords, harmony, start_index, end_index, prev_harmony_unit){
+		var change;
+		if(start_index > end_index){
+			change = -1;
+		}
+		else{
+			change = 1;
+		}
+		
+	}
+	generate_cadence_harmony(chords, harmony, cadence){
+		
+	}
+	create_empty_harmony(length){
+		var harmony = [];
+		for(var i = 0; i < length; i++){
+			harmony.push({0: new Voice(null), 1: new Voice(null), 2: new Voice(null), 3: new Voice(null)});
+		}
+		return harmony;
+	}
+	generate_harmony(chords, prev_harmony_unit, phrase_plan){
+		if(phrase_plan.get_previous_cadence_chord() == null){
+			
+		}
+		else{
+			
+		}
+		
+		var harmony = [this.create_empty_harmony(chords.length), this.create_empty_harmony(chords.length - 2)];
+		var start_index = [chords.length - 3, 0];
+		var end_index = [0, chords.length - 3];
+		this.generate_cadence_harmony(chords, harmony[0], phrase_plan.get_cadence());
+		
+		this.complete_harmony(chords, harmony[0], start_index[0], end_index[0], harmony[0][chords.length - 2]);
+		// if harmony[0] can be stitched to prev_harmony_unit
+		if(false){
+			return harmony[0];
+		}
+		
+		this.complete_harmony(chords, harmony[1], start_index[1], end_index[1], prev_harmony_unit);
 		
 	}
 }
