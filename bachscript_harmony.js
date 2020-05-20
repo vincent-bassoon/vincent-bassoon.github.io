0ed3f280be18ed95e44f3036d699b855bdbf3468
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-05-19 21:13:26 -0400

    Update bachscript_harmony.js

--- bachscript_harmony.js
+++ bachscript_harmony.js
@@ -23,25 +23,86 @@ class HarmonyFunctions {
 					      "pacm": ac_probs};
 		
 		this.cadence_indicies = [];
-		
-		
+		this.pitch_options = {};
+		for(var voice = 0; voice < 4; voice++){
+			this.pitch_options[voice] = {};
+			for(var degree = 0; degree < 4; degree++){
+				this.pitch_options[voice][degree] = [];
+			}
+		}
+	}
+	reset_pitch_options(){
+		for(var voice = 0; voice < 4; voice++){
+			for(var degree = 0; degree < 4; degree++){
+				this.pitch_options[voice][degree] = [];
+			}
+		}
 	}
 	is_cadence(index){
 		return this.cadence_indicies.includes(index);
 	}
-	value_in_pref_range(pitch, voice_index){
+	is_in_absolute_range(value, voice_index){
+		return value >= this.absolute_ranges[voice_index].min && 
+			value <= this.absolute_ranges[voice_index].max;
+	}
+	get_pitch_in_pref_range(pitch, voice_index){
 		var min = this.preferred_ranges[voice_index].min;
 		while(pitch < min){
 			pitch += 12;
 		}
 		return pitch;
 	}
-	generate_adjacent(chords, harmony, existing_harmony, target_index, adjacent_index){
-		if(Math.abs(adjacent_index - target_index) != 1){
-			console.log("index error");
-			return;
+	get_pitch_closest_to(pitch, static_value){
+		while(Math.abs(static_value - pitch) > 6){
+			pitch += 12;
+		}
+		return pitch;
+	}
+	generate_single_harmony(chords, harmony, index, fixed_pitches){
+		this.reset_pitch_options();
+		var options = this.pitch_options;
+		
+		var pitches = [];
+		for(var degree = 0; degree < 3; degree++){
+			pitches[degree] = this.note_functions.get_pitch(chords[index], degree);
 		}
 		
+		for(var voice = 0; voice < 4; voice++){
+			if(fixed_pitches[voice].index == index){
+				options[voice][fixed_pitches[voice].degree].push(fixed_pitches[voice].pitch);
+			}
+			else{
+				var prev_note = harmony[index + 1][voice].get_start_value();
+				for(var degree = 0; degree < 3; degree++){
+					if(index == 0){
+						var value = this.get_pitch_in_pref_range(pitches[degree], prev_note);
+						options[voice][degree].push(value);
+					}
+					else{
+						var value = this.get_pitch_closest_to(pitches[degree], prev_note);
+						if(this.is_in_absolute_range(value, voice)){
+							options[voice][degree].push(value);
+						}
+					}
+				}
+			}
+		}
+		if(this.fill_harmony(harmony, [0, 0, 1, 2], options, index, 0)){
+			return;
+		}
+		if(this.fill_harmony(harmony, [0, 1, 1, 2], options, index, 0)){
+			return;
+		}
+		if(this.fill_harmony(harmony, [0, 1, 2, 2], options, index, 0)){
+			return;
+		}
+		console.log("failure at index ", index);
+	}
+	has_errors(harmony, index, order_index){
+		if(order_index == 0){
+			return false;
+		}
+		return false;
 	}
 	fill_harmony(harmony, voicing, pitch_options, index, order_index){
 		if(order_index == 4){
@@ -52,13 +113,20 @@ class HarmonyFunctions {
 			for(var j = 0; j < pitch_options[voice][i].length; j++){
 				harmony[index][voice].set_note(pitch_options[voice][i][j]);
 				var num = voicing.shift();
-				if(this.fill_harmony(harmony, voicing, pitch_options, index, order_index + 1)){
+				if(this.has_errors(harmony, index, order_index)){
+					harmony[index][voice].set_note(null);
+					voicing.push(num);
+				}
+				else if(this.fill_harmony(harmony, voicing, pitch_options, index, order_index + 1)){
 					return true;
 				}
-				harmony[index][voice].set_note(null);
-				voicing.push(num);
+				else{
+					harmony[index][voice].set_note(null);
+					voicing.push(num);
+				}
 			}
 		}
+		return false;
 	}
 	create_empty_harmony(length){
 		var harmony = [];
@@ -80,18 +148,22 @@ class HarmonyFunctions {
 			this.cadence_indicies.push(sum);
 			
 			var num = choose_int(this.cadence_probabilities[chorale_plan[i].get_cadence()]);
+			var degree = chord_array[i][chord_array[i].length - 1].get_degree(num);
 			var pitch = nf.num_to_pitch_for_cad(num, chord_array[i][chord_array[i].length - 1]);
-			fixed_pitches[3].unshift({"pitch": pitch, "index": sum});
+			fixed_pitches[3].unshift({"pitch": pitch, "degree": degree, "index": sum});
 		}
 		
 		var harmony = this.create_empty_harmony(chords.length);
 		for(var i = 0; i < chords.length; i++){
 			var inversion = chords[i].get_inversion();
 			if(inversion != null){
-				fixed_pitches[0].unshift({"pitch": nf.get_bass_pitch(chords[i]), "index": i});
+				fixed_pitches[0].unshift({"pitch": nf.get_bass_pitch(chords[i]), "degree": inversion, "index": i});
 			}
 		}
 		
-		
+		for(var i = 0; i < chords.length; i++){
+			this.generate_single_harmony(chords, harmony, i, fixed_pitches);
+		}
+		return harmony;
 	}
 }
