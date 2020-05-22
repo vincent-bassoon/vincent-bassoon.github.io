695bea12bb75570e98201b89759cf06562287e81
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-05-22 14:47:12 -0400

    Update bachscript_score.js

--- bachscript_score.js
+++ bachscript_score.js
@@ -94,12 +94,7 @@ class Score {
 		
 		this.names_in_key = note_functions.get_names_in_key(key_temp);
 		
-		if(key_temp.get_modality() == "minor"){
-			this.key_name = note_functions.value_to_name(3, key_temp);
-		}
-		else{
-			this.key_name = note_functions.value_to_name(0, key_temp);
-		}
+		this.key_name = note_functions.value_to_name(key_temp.get_pitch(), key_temp);
 		
 		this.harmony = harmony;
 		this.chords = chords;
