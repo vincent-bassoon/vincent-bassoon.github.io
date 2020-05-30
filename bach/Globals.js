20a3b2b07a9d63b89e9d202a43424f978c846bde
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-05-29 23:04:43 -0400

    Update Globals.js

--- bach/Globals.js
+++ bach/Globals.js
@@ -1,3 +1,30 @@
+function watchForHover(){
+	
+	let lastTouchTime = 0
+
+	function enableHover(){
+		if(new Date() - lastTouchTime < 500){
+			return document.body.classList.add('hasHover');
+		}
+	}
+	
+	function disableHover(){
+		document.body.classList.remove('hasHover');
+	}
+
+	function updateLastTouchTime(){
+		lastTouchTime = new Date();
+	}
+
+	document.addEventListener('touchstart', updateLastTouchTime, true);
+	document.addEventListener('touchstart', disableHover, true);
+	document.addEventListener('mousemove', enableHover, true);
+
+	enableHover();
+}
+
+watchForHover();
+
 function choose(probs){
 	var num = Math.random();
 	var choice = null;
