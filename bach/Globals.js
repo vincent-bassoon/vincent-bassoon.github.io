24dff8576422392488c7aef3c6be89aba673c691
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2020-06-08 14:12:47 -0400

    Update Globals.js

--- bach/Globals.js
+++ bach/Globals.js
@@ -238,6 +238,8 @@ class Chord {
 
 class ScoreUnit {
 	constructor(target_avgs, harmony){
+		this.scores = [null, null, null, null];
+		
 		this.avgs = [0, 0, 0, 0];
 		this.avg_nums = [0, 0, 0, 0];
 		this.target_avgs = target_avgs;
@@ -252,14 +254,26 @@ class ScoreUnit {
 			this.avgs[voice] = next_harmony.getNextAvg(voice, value);
 		}
 	}
+	getAvgScore(next_score, voice, value){
+		if(voice != 0 || this.harmony.end_of_phrase){
+			return 0;
+		}
+		var score = 0;
+		var target_avg = this.target_avgs[voice];
+		var avg = next_score.getNextAvg(voice, value);
+		var diff = Math.abs(target_avg - avg);
+		if(diff > 2){
+			if((value > avg && avg > target_avg) || (value < avg && avg < target_avg)){
+				score += diff * 5;
+			}
+		}
+		if(Math.abs(value - target_avg) > 5){
+			score += 20;
+		}
+		return score;
+	}
 	getNextAvg(voice, value){
 		return (this.avgs[voice] * this.avg_nums[voice] + value) / (this.avg_nums[voice] + 1);
-	}
-	getAvgNum(voice){
-		return this.avg_nums[voice];
-	}
-	getTargetAvg(voice){
-		return this.target_avgs[voice];
 	}
 	addToHistory(){
 		var copy = [[], [], []];
@@ -290,15 +304,15 @@ class ScoreUnit {
 }
 
 class HarmonyUnit {
-	constructor(target_avgs, is_end_of_phrase){
+	constructor(chord, target_avgs, end_of_phrase){
 		this.values = [[null, null, null, null], [null, null, null, null], [null, null, null, null]];
 		this.leap = [null, null, null, null];
 		
-		this.chord = null;
+		this.chord = chord;
 		
-		this.is_end_of_phrase = is_end_of_phrase;
+		this.end_of_phrase = end_of_phrase;
 		
-		this.score_unit = new ScoreUnit(target_avgs, this);
+		this.score = new ScoreUnit(target_avgs, this);
 	}
 	getNumNotes(voice){
 		if(this.values[1][voice] != this.values[2][voice]){
@@ -348,15 +362,15 @@ class NoteFunctions {
 		this.interval_mapping = {0: 0, 1: 1.5, 2: 3.5, 3: 5, 4: 7, 5: 8.5, 6: 10.5};
 		
 		
-		this.preferred_ranges = {0: {"min": 5 + 24, "max": 4 + 36},
-					 1: {"min": 5 + 36, "max": 4 + 48},
-					 2: {"min": 9 + 36, "max": 8 + 48},
-					 3: {"min": 4 + 48, "max": 3 + 60}},
+		this.preferred_ranges = {0: {"min": 4 + 48, "max": 3 + 60},
+					 1: {"min": 9 + 36, "max": 8 + 48},
+					 2: {"min": 5 + 36, "max": 4 + 48},
+					 3: {"min": 5 + 24, "max": 4 + 36}};
 	
-		this.absolute_ranges = {0: {"min": 0 + 24, "max": 0 + 48},
-					1: {"min": 0 + 36, "max": 7 + 48},
-					2: {"min": 7 + 36, "max": 2 + 60},
-					3: {"min": 0 + 48, "max": 7 + 60}};
+		this.absolute_ranges = {0: {"min": 0 + 48, "max": 7 + 60},
+					1: {"min": 7 + 36, "max": 2 + 60},
+					2: {"min": 0 + 36, "max": 7 + 48},
+					3: {"min": 0 + 24, "max": 0 + 48}};
 	}
 	valueToSimpleName(value){
 		return this.simple_val_to_name[value % 12];
@@ -379,14 +393,14 @@ class NoteFunctions {
 		}
 		return Math.abs(Math.abs(change) - this.interval_mapping[letter_index_diff]) > 0.5;
 	}
-	inAbsoluteRange(value, voice_index){
-		return value >= this.absolute_ranges[voice_index].min && value <= this.absolute_ranges[voice_index].max;
+	inAbsoluteRange(value, voice){
+		return value >= this.absolute_ranges[voice].min && value <= this.absolute_ranges[voice].max;
 	}
-	inPrefRange(value, voice_index){
-		return value >= this.preferred_ranges[voice_index].min && value <= this.preferred_ranges[voice_index].max;
+	inPrefRange(value, voice){
+		return value >= this.preferred_ranges[voice].min && value <= this.preferred_ranges[voice].max;
 	}
-	getValueInPrefRange(pitch, voice_index){
-		var min = this.preferred_ranges[voice_index].min;
+	getValueInPrefRange(pitch, voice){
+		var min = this.preferred_ranges[voice].min;
 		while(pitch < min){
 			pitch += 12;
 		}
