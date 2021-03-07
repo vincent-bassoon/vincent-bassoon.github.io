29350fff25cdc7058bed67656cefff1f3cd6dacf
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2021-03-07 01:01:53 -0500

    Update firebase-messaging-sw.js

--- clue/firebase-messaging-sw.js
+++ clue/firebase-messaging-sw.js
@@ -16,18 +16,3 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
