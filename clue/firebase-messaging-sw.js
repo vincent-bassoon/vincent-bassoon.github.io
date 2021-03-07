4bd986b51bbe212698c8b4fc967f2e11cb7962bb
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2021-03-06 23:24:23 -0500

    sw

---
+++ clue/firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+var firebaseConfig = {
+    apiKey: "AIzaSyD9njoN659jBaO-Ov6sQI33Q5xi9kRl3jw",
+    authDomain: "clue-51aa1.firebaseapp.com",
+    databaseURL: "https://clue-51aa1-default-rtdb.firebaseio.com",
+    projectId: "clue-51aa1",
+    storageBucket: "clue-51aa1.appspot.com",
+    messagingSenderId: "314308110149",
+    appId: "1:314308110149:web:73d763dd4c6d554714802c",
+    measurementId: "G-092MCPQDB9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
