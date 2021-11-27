2cfb4bb398f61750b2374c0873b86226ca301e17
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2021-11-27 16:57:31 -0600

    clam

---
+++ clam/script.js
@@ -0,0 +1,37 @@
+var countDownDate = new Date("Dec 3, 2021 19:00:00").getTime();
+var done = false;
+
+var x = setInterval(function() {
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("timer").innerHTML = "The Clam Has Arrived";
+		return;
+	}
+
+	var units = {};
+	units[0] = Math.floor(distance / (1000 * 60 * 60 * 24));
+	units[1] = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	units[2] = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	units[3] = Math.floor((distance % (1000 * 60)) / 1000);
+
+	names = ["day", "hour", "minute", "second"];
+	var display = "";
+	for(var i = 0; i < 4; i++){
+		display += units[i] + " " + names[i];
+		if(units[i] != 1){
+			display += "s";
+		}
+		if(i != 3){
+			display += ", ";
+		}
+	}
+
+    document.getElementById("timer").innerHTML = "The Clam will arrive in <p></p><p></p> " + display + "<p></p><p></p>";
+	if(!done){
+		done = true;
+		document.getElementById("loading-container").style.opacity = 1;
+	}
+}, 1000);
