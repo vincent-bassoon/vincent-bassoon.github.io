356232b0c07e821288e41f570561c787ea77cf1f
vincent-bassoon <53240134+vincent-bassoon@users.noreply.github.com>
2022-11-23 22:03:20 -0800

    Update script.js

--- clam/script.js
+++ clam/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Nov 22, 2022 14:15:00").getTime();
+var countDownDate = new Date("Dec 12, 2022 18:05:00 GMT-0500").getTime();
 var done = false;
 
 var x = setInterval(function() {
